93c26eb04893009916d9fee0222a2d8359c0ef11
emsfur <sherwinfernandes101@gmail.com>
2023-12-17 19:22:49 -0800

    refactored part B to run in a few seconds

--- day 5/partA.py
+++ day 5/partA.py
@@ -21,21 +21,26 @@ for mapData in data[1: len(data)]:
     info = mapData.split(":") 
     numData = info[1].split("\n")
     numData = [[int(num) for num in val.split(" ")] for val in numData[1: len(numData)]]
+    # creates a map containing all conversion ranges tied to conversion type
     conversionMap[info[0]] = numData
 
 def convert(key, val):
     mapData = conversionMap[key]
-
+    # go through all conversion ranges based on key and return converted value
     for bound in mapData:
         if (val >= bound[1] and val < bound[1] + bound[2]):
             return val + (bound[0] - bound[1])
+    # return same output value if not in any conversion ranges
     return val
 
+# go through each seed individually
 for seed in seeds:
+    # use as variable that goes through each conversion one by one
     output = seed
     for key in conversionMap.keys():
-        output = convert(key, output)
+        output = convert(key, output) # convert to next type
 
+    # update the min location if found or set as min on first iteration
     if (minLocation == None or output < minLocation):
         minLocation = output
 
--- day 5/partB.py
+++ day 5/partB.py
@@ -14,8 +14,9 @@ data[0] = data[0].strip("seeds: ")
 seeds = [int(val) for val in data[0].split(" ")]
 conversionMap = {}
 minLocation = None
-counter = 0
-max = 0
+
+inputQueue = []
+outputQueue = []
 
 # uses the rest of the input data to populate the conversion map
 for mapData in data[1: len(data)]:
@@ -25,40 +26,51 @@ for mapData in data[1: len(data)]:
     numData = [[int(num) for num in val.split(" ")] for val in numData[1: len(numData)]]
     conversionMap[info[0]] = numData
 
-def convert(key, val):
+# populates the inputQueue with seed values
+for i in range(0, len(seeds), 2):
+    inputQueue.append( (seeds[i], seeds[i]+seeds[i+1]-1) )
+
+# goes through each conversion type to convert all ranges at once (all seedRanges -> all fertilizerRanges)
+for key in conversionMap.keys():
     mapData = conversionMap[key]
 
-    for bound in mapData:
-        if (val >= bound[1] and val < bound[1] + bound[2]):
-            return val + (bound[0] - bound[1])
-    return val
+    while not len(inputQueue) == 0:
+        seedBound = inputQueue.pop(0)
+        seedMin = seedBound[0]
+        seedMax = seedBound[1]
 
-def getLocation(seed):
-    output = seed
-    for key in conversionMap.keys():
-            output = convert(key, output)
+        converted = False
+        for bound in mapData:
+            offset = bound[0]-bound[1]
+            boundMin = bound[1]
+            boundMax = boundMin + bound[2] - 1
+
+            # conversion logic available in excalidraw file
+            if (seedMin >= boundMin and seedMax <= boundMax): 
+                outputQueue.append( (seedMin+offset, seedMax+offset) )
+                converted = True
+                break # don't need to check the other bounds when 
+            
+            elif (seedMin >= boundMin and seedMin <= boundMax) and seedMax > boundMax: 
+                outputQueue.append( (seedMin+offset, boundMax+offset) )
+                inputQueue.append( (boundMax+1, seedMax) ) # unconverted ranges sent into input if it possibly converts
+                converted = True
+        
+            elif seedMin < boundMin and (seedMax >= boundMin and seedMax <= boundMax):
+                inputQueue.append( (seedMin, boundMin-1) )
+                outputQueue.append( (boundMin+offset, seedMax+offset) )
+                converted = True
+        
+        # if it went through all conversion ranges and didn't convert, send it to the next conversion as is
+        if (not converted):
+            outputQueue.append( (seedMin, seedMax) )
     
-    return output
+    inputQueue = outputQueue[:]
+    outputQueue = []
 
-for i in range(0, len(seeds), 2):
-    max += seeds[i+1]
+min = inputQueue[0][0]
+for ranges in inputQueue:
+    if (ranges[0] < min):
+        min = ranges[0]
 
-
-for i in range(0, len(seeds), 2):
-    output = ""
-    for j in range(seeds[i], seeds[i] + seeds[i+1]):
-        output = getLocation(j)
-
-        if (minLocation == None or output < minLocation):
-            minLocation = output
-
-        counter += 1
-        print(str((counter/max) * 100)  + "%")
-
-    
-
-
-print(minLocation)
-
-
-# issue right now is that it goes through all the numbers within the range 
+print(min)
